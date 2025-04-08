a1f5e3737a0c18599fe27c4d8e1abc843ef637aa
Esteban Pineda <estebanpineda527@gmail.com>
2025-04-07 19:47:40 -0500

    nueva corrección del crud Sentido

--- Models/TipoIndicadorModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FrontBlazor.Models
-{
-    public class TipoIndicadorModel
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-    }
-}
--- Models/UnidadMedicionModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FrontBlazor.Models
-{
-    public class UnidadMedicionModel
-    {
-        public int Id { get; set; }
-        public string Descripcion { get; set; }
-    }
-}
